75573c679d454781d86fadf0802418e3ba9c257b
ujifgc <ujifgc@gmail.com>
2012-05-14 17:11:45 +0400

    uub parser

--- app/helpers/core_helper.rb
+++ app/helpers/core_helper.rb
@@ -21,33 +21,62 @@ Swift.helpers do
   def fragment( name, *args )
     partial 'fragments/'+name, *args
   end
+  
+  def parse_vars( str )
+    args = []
+    hash = {}
+    # 0 for element name
+    #             0              12             3    4            5             6
+    vars = str.scan( /["']([^"']+)["'],?|(([\S^,]+)\:\s*(["']([^"']+)["']|([^,'"\s]+)))|([^,'"\s]+),?/ ) #"
+    vars.each do |v|
+      case
+      when v[0]
+        args << v[0].to_s
+      when v[1] && v[4]
+        hash.merge! v[2].to_sym => v[4]
+      when v[1] && v[5]
+        hash.merge! v[2].to_sym => v[5]
+      when v[6]
+        args << v[6]
+      end
+    end
+    [args, hash]    
+  end
+
+  def parse_code( html, args, content = '' )
+    html.gsub(/\[(\d+)\]|\[(content)\]/) do |s|
+      tag = $1
+      if tag.to_i > 0
+        args[tag.to_i-1]
+      elsif $2 == 'content'
+        content
+      else
+        "[#{tag}]"
+      end
+    end
+  end
 
   def parse_content( str )
     @parse_level = @parse_level.to_i + 1
     return t(:parse_level_too_deep)  if @parse_level > 3
+    needs_capturing = false
 
     str.gsub!(/\[(.*?)\]/) do |s|
       tag = $1
       md = tag.match /(page|link|block|text|image|img|file|asset|element|elem|lmn)\s+(.*)/
-      next "[#{tag}]"  unless md
-      args = []
-      hash = {}
-      type = md[1] #         0              12             3    4            5             6
-      vars = md[2].scan /["']([^"']+)["'],?|(([\S^,]+)\:\s*(["']([^"']+)["']|([^,'"\s]+)))|([^,'"\s]+),?/
-      # 0 for element name
-      # "
-      vars.each do |v|
-        case
-        when v[0]
-          args << v[0].to_s
-        when v[1] && v[4]
-          hash.merge! v[2].to_sym => v[4]
-        when v[1] && v[5]
-          hash.merge! v[2].to_sym => v[5]
-        when v[6]
-          args << v[6]
+      unless md
+        tags = tag.partition ' '
+        code = Code.by_slug tags[0]  unless tags[0][0] == '/'
+        if code && code.is_single
+          args, hash = parse_vars tags[2]
+          next parse_code( code.html, args )
+        else
+          needs_capturing = true  if code
+          next "[#{tag}]"
         end
       end
+      type = md[1]
+      args, hash = parse_vars md[2]
       if hash[:title].blank?
         newtitle = if ['element', 'elem', 'lmn'].include?( type )
           args[2..-1]
@@ -68,6 +97,14 @@ Swift.helpers do
       when 'element', 'elem', 'lmn'
         element *args, hash
       end.strip
+    end
+    if needs_capturing
+      logger << str
+      str.gsub!( /\[([^\s]*)(.*)\](.*)\[\/(\1)\]/ ) do |s|
+        args, hash = parse_vars $2
+        code = Code.by_slug $1
+        parse_code( code.html, args, $3 )
+      end
     end
     @parse_level -= 1
     str
