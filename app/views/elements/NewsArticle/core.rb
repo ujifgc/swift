3df29f19e0095d1ec58bdb669ba9f072bcd6bcdb
Viktor Teptin <burduker@gmail.com>
2013-06-19 16:41:14 +0400

    show new.info if new.text blank

--- app/views/elements/NewsArticle/core.rb
+++ app/views/elements/NewsArticle/core.rb
@@ -1,4 +1,5 @@
 @new = NewsArticle.by_slug @swift[:slug]
 not_found  unless @new
+@new.text = @new.info  if @new.text.blank?
 @swift[:path_pages][-1] = Page.new :title => @new.title
 @swift[:resource] = @new
