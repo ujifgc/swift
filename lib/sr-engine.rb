0b50e3ad0a0937ff5460e74c1784c4c424f949f8
ujifgc <ujifgc@gmail.com>
2013-06-20 12:05:19 +0400

    refactor #element

--- lib/sr-engine.rb
+++ lib/sr-engine.rb
@@ -90,23 +90,31 @@ module Padrino
         end
       end
 
+      def fill_identity( opts = {} )
+        identity = { :class => opts[:name] }
+        identity[:id] = opts[:id]                                 if opts[:id]
+        identity[:class] += " #{opts[:class]}"                    if opts[:class]
+        identity[:class] += " #{opts[:name]}-#{opts[:instance]}"  if opts[:instance]
+        identity
+      end
+
       def element( name, *args )
         @opts = args.last.kind_of?(Hash) ? args.pop : {}
         @args = args
-        return defer_element( name, @args, @opts )  if DEFERRED_ELEMENTS.include?(name) && @opts[:process_defer].nil?
-        template = "elements/#{name}/view"
 
-        @identity = { :class => "#{name}" }
-        @identity[:id] = @opts[:id]  if @opts[:id]
-        @identity[:class] += ' ' + @opts[:class]  if @opts[:class]
+        return defer_element( name, @args, @opts )  if DEFERRED_ELEMENTS.include?(name) && @opts[:process_defer].nil?
+
+        @opts[:name] = name
+        @identity = fill_identity @opts
+
+        core_rb = "#{Swift.views}/elements/#{name}/core.rb"
+        template = "elements/#{name}/view"
         if @opts[:instance]
-          @identity[:class] += " #{name}-#{@opts[:instance]}"
           instance = "#{template}-#{@opts[:instance]}"
           template = instance  if File.exists?( "#{Swift.views}/#{instance}.slim" ) 
         end
 
         catch :output do
-          core_rb = "#{Swift.views}/elements/#{name}/core.rb"
           binding.eval File.read(core_rb), core_rb  if File.exists?(core_rb)
           render nil, template, :layout => false
         end
