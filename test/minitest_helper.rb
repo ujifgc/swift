b91b20a5bb5c0e9ff4f8c9f6e3b1528014f2a728
Igor Bochkariov <ujifgc@gmail.com>
2013-07-12 14:24:44 +0400

    cleanup tests

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,2 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'minitest/autorun'
 require File.expand_path('../../config/boot.rb', __FILE__)
-require 'slim'
-require 'swift'
-
+require 'minitest/autorun'
