60bede9f7d1b7856e28aa1ad494aadd1d1b9ad56
Igor Bochkariov <ujifgc@gmail.com>
2013-07-12 15:46:04 +0400

    cleanup boot.rb

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,3 @@
-#coding:utf-8
-
 DataMapper::Logger.new( 'log/sql.log', Padrino.env == :development ? :debug : :error )
 DataMapper::Property::String.length(255)
-
 DataMapper.setup :default, YAML.load_file( Padrino.root('config/database.yml') )[PADRINO_ENV]
