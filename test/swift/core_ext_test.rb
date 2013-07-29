258ef5e8b970d2303ab6714650714bf70e5a1239
Igor Bochkariov <ujifgc@gmail.com>
2013-07-29 12:57:50 +0400

    add some tests

--- test/swift/core_ext_test.rb
+++ test/swift/core_ext_test.rb
@@ -1,16 +1,33 @@
 require 'minitest_helper'
 
-describe 'String' do
+describe String do
   it 'should concat with /' do
     ('foo' / 'bar').must_equal 'foo/bar'
   end
+
   it 'should squeeze /' do
     ('foo' / '/bar').must_equal 'foo/bar'
   end
+
+  it 'should slugize a string' do
+    'коко лоло ё'.as_slug.must_equal 'koko-lolo-yo'
+  end
+
+  it 'should convert cyrillic case' do
+    'коко ЛОЛО ё'.case(:up).must_equal 'КОКО ЛОЛО Ё'
+    'коко ЛОЛО ё'.case(:lo).must_equal 'коко лоло ё'
+    'коко ЛОЛО ё'.case(:cap).must_equal 'Коко лоло ё'
+  end
 end
 
-describe 'Symbol' do
+describe Symbol do
   it 'should concat with /' do
     (:foo / :bar).must_equal 'foo/bar'
+  end
+end
+
+describe NilClass do
+  it 'should appear empty' do
+    nil.any?.must_equal nil
   end
 end
