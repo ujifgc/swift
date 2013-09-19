07c13c2fff5fd8034812d7754690913d529eda73
Igor Bochkariov <ujifgc@gmail.com>
2013-09-19 10:41:35 +0400

    cleanup, remove tilt-haml patch

--- lib/swift/monkey/rack.rb
+++ lib/swift/monkey/rack.rb
@@ -1,5 +1,3 @@
-require 'securerandom'
-
 module Rack
   # allow params keys to be utf-8 in multipart mode
   module Utils
@@ -7,6 +5,7 @@ module Rack
       unless instance_methods.map(&:to_s).include?('old_to_params_hash')
         alias_method :old_to_params_hash, :to_params_hash
       end
+
       def to_params_hash
         Hash[old_to_params_hash.map{ |k,v| [k.dup.force_encoding('utf-8'), v] }]
       end
@@ -15,9 +14,10 @@ module Rack
 
   # Adds `addr` method to get ip address of a client
   class Request
-      def parse_multipart(env)
-        Rack::Multipart.parse_multipart(env)
-      end
+    def parse_multipart(env)
+      Rack::Multipart.parse_multipart(env)
+    end
+
     def addr
       @env['HTTP_X_FORWARDED_FOR'] || @env['REMOTE_ADDR']
     end
