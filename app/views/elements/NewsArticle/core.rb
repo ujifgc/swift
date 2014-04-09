6b1c1ce1d9f70c5a18162678baf326dbb4d84139
Igor Bochkariov <ujifgc@gmail.com>
2014-04-09 16:50:39 +0400

    cleanup thesis miscommit

--- app/views/elements/NewsArticle/core.rb
+++ app/views/elements/NewsArticle/core.rb
@@ -3,5 +3,3 @@ not_found  unless @article
 @article.text = @article.info  if @article.text.blank?
 swift.path_pages[-1] = Page.new :title => @article.title
 swift.resource = @article
-
-@board = @article.dissertation.board
