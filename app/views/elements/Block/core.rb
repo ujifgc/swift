632bf0a092045cbfd81a4c2f2b2a2d40bb3c2944
Igor Bochkariov <ujifgc@gmail.com>
2014-08-20 14:50:31 +0400

    alow notitle tables

--- app/views/elements/Block/core.rb
+++ app/views/elements/Block/core.rb
@@ -1,11 +1,13 @@
 block = Block.by_slug @args[0]
 throw :output, "[Block ##{@args[0]} missing]"  unless block
-@text = case
-when Block::Types[block.type] == :html
-  # this ensures that tagged HTML content is brought directly to response body without escaping
-  block.text.html_safe
-when Block::Types[block.type] == :table && !@identity[:class].index('notitle')
-  engine_render block.text.gsub(/<table(.*?)>/m, "<table\\1>\n<caption>#{@opts[:title]||block.title}</caption>")
-else
-  engine_render block.text
-end
+@text =
+  case
+  when Block::Types[block.type] == :html
+    # this ensures that tagged HTML content is brought directly to response body without escaping
+    block.text.html_safe
+  when Block::Types[block.type] == :table && !@identity[:class].index('notitle')
+    block.text.gsub!(/<table(.*?)>/m, "<table\\1>\n<caption>#{@opts[:title]||block.title}</caption>") unless @opts[:instance] == :notitle
+    engine_render block.text
+  else
+    engine_render block.text
+  end
