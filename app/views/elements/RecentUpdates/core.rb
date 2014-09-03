54aa0955eaad36f420f272cabcf5efcfd7e5c258
Igor Bochkariov <ujifgc@gmail.com>
2014-09-03 13:46:17 +0400

    add RecentUpdates element

---
+++ app/views/elements/RecentUpdates/core.rb
@@ -0,0 +1,8 @@
+﻿models = @opts[:models] || [Page, NewsArticle]
+limit = @opts[:limit] || 5
+
+@updated_objects = []
+models.each do |model|
+  @updated_objects += model.published.all( :order => :updated_at ).last(limit).to_a
+end
+@updated_objects = @updated_objects.sort_by{ |object| -object.updated_at.to_i }.first(limit)
