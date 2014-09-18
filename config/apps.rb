a3509a18fb745912905aa2482a468d45142bf46c
Igor Bochkariov <ujifgc@gmail.com>
2014-09-18 15:27:12 +0400

    rename codebase

--- config/apps.rb
+++ config/apps.rb
@@ -1,6 +1,6 @@
 Padrino.configure_apps do
-  prerequisites << Padrino.root('../faculty/models/*.rb')
-  prerequisites << Padrino.root('../faculty/lib/**/*.rb')
+  prerequisites << Padrino.root('../base/models/*.rb')
+  prerequisites << Padrino.root('../base/lib/**/*.rb')
 
   set :session_secret, 'fd3a32d61be75844ffaf63deaeff410cd4d686f0de3154f4497a3cab0d493289'
 
@@ -12,5 +12,5 @@ Padrino.configure_apps do
   end
 end
 
-Padrino.mount("Admin", :app_file => Padrino.root('../faculty/admin/app.rb')).to("/admin")
+Padrino.mount("Admin", :app_file => Padrino.root('../base/admin/app.rb')).to("/admin")
 Padrino.mount("Swift::Application").to('/')
